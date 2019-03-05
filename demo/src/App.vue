a71b0a14a733cfa153dd35388b53b28b2a0b5870
Jordan Caudill <jacaudill74@gmail.com>
2019-03-04 23:15:01 -0600

    demo cleanup

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,35 +1,51 @@
 <style lang="scss">
-$flexboy: (md: 1280px, lg: 1600px);
-// $containerWidths: (600, 900, 1200);
+$flexboy: (
+  md: 1280px,
+  lg: 1600px
+);
+$flexboyContainers: (
+  default: 600px,
+  md: 900px,
+  lg: 1200px
+);
 @import "../lib/_flex.scss";
+#app {
+  //   @extend .row;
+}
 #controls {
-//   display: flex;
-//   flex-direction: row;
-  // align-items: center;
-  @extend %row-md;
-  // @include desktop(('.row'));
-  .type-section {
-    // background: rgba(0, 0, 255, 0.2);\
+  @extend .row, .wrap, .j-around;
+
+  .control-type {
     padding: 10px;
+	width: 200px;
+	border: solid 2px gray;
     .control {
-      height: 40px;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: flex-end;
-      label {
-        // display: block;
-        user-select: none;
+      //   height: 40px;
+	  //   @extend .row, .j-end, .a-center;
+	  &.even {
+		  background: lightblue;
+	  }
+      .size {
+		  height: 30px;
         font-family: Helvetica, sans-serif;
-      }
-      input[type="checkbox"] {
-        width: 20px;
-        height: 20px;
+        color: gray;
+        &:first-child {
+          font-size: 20px;
+          color: black;
+          input[type="checkbox"] {
+            width: 20px;
+            height: 20px;
+          }
+        }
+        label {
+          user-select: none;
+        }
       }
     }
   }
 }
 #parent {
+  width: 100%;
   background: rgba(255, 0, 0, 0.1);
   margin-top: 30px;
 }
@@ -62,23 +78,37 @@ body {
 
 <template>
   <div id="app">
-    <section id="breakpoints">
-      <form id="new-breakpoint" onsubmit="addBreakpoint()">
-        <label>New Breakpoint</label>
-        <input placeholder="Breakpoint name (ex: md)" type="text" id="new-breakpoint-name">
-        <input
-          placeholder="Breakpoint width (ex: 1280)"
-          type="number"
-          min="1"
-          id="new-breakpoint-size"
-        >
-        <button type="submit">Add</button>
-      </form>
+    <div>
+      <section id="breakpoints">
+        <form id="new-breakpoint" onsubmit="addBreakpoint()">
+          <label>New Breakpoint</label>
+          <input placeholder="Breakpoint name (ex: md)" type="text" id="new-breakpoint-name">
+          <input
+            placeholder="Breakpoint width (ex: 1280)"
+            type="number"
+            min="1"
+            id="new-breakpoint-size"
+          >
+          <button type="submit">Add</button>
+        </form>
+      </section>
+      <section id="controls">
+        <div class="control-type" :key="type" v-for="type in types">
+          <div class="control" :class="{even: idx % 2 === 0}" :key="control" v-for="(control, idx) in classes[type]">
+            <div class="size" :key="size.size" v-for="size in sizes">
+              <label v-if="!size.name" :for="control">.{{control}}</label>
+              <label v-if="size.name" :for="control">.{{control}}-{{size.name}}</label>
+              <input type="checkbox" :name="control">
+            </div>
+          </div>
+        </div>
+      </section>
+      <button @click="children > 1? children-- : null;">Remove child</button>
+      <button @click="children < 10? children++ : null;">Add child</button>
+    </div>
+    <section id="parent">
+      <div class="child" :key="child" v-for="child in children">{{child}}</div>
     </section>
-    <section id="controls"></section>
-    <button onclick="deleteChild()">Remove child</button>
-    <button onclick="addChild()">Add child</button>
-    <section id="parent"></section>
   </div>
 </template>
 
@@ -86,89 +116,48 @@ body {
 export default {
   name: "app",
   components: {},
-  mounted: function() {
-    const parent = document.getElementById("parent");
-    const types = [
-      "direction",
-      "justify",
-      "align",
-      "container",
-      "wrap",
-      "hide",
-      "show",
-      "flex",
-      "only"
-    ];
-    const classes = {
-      direction: ["row", "col", "row-reverse", "col-reverse"],
-      justify: ["j-start", "j-end", "j-center", "j-between", "j-around"],
-      align: ["a-start", "a-end", "a-center", "a-baseline", "a-stretch"],
-      container: ["container", "fluid"],
-      wrap: ["wrap", "wrap-reverse", "nowrap"],
-      hide: ["hide", "hide-at"],
-      show: ["show", "show-at"],
-      flex: ["flex", "flex-at"],
-      only: ["only"]
+  data: function() {
+    return {
+      sizes: [
+        { name: "", size: 0 },
+        { name: "md", size: 1280 },
+        { name: "lg", size: 1600 }
+      ],
+      children: 5,
+      classes: {
+        direction: ["row", "col", "row-reverse", "col-reverse"],
+        justify: ["j-start", "j-end", "j-center", "j-between", "j-around"],
+        align: ["a-start", "a-end", "a-center", "a-baseline", "a-stretch"],
+        container: ["container", "fluid"],
+        wrap: ["wrap", "wrap-reverse", "nowrap"],
+        hide: ["hide"],
+        only: ["only"]
+      },
+      types: [
+        "direction",
+        "justify",
+        "align",
+        "container",
+        "wrap",
+        "hide",
+        "only"
+      ]
     };
-    let breakpoints = {};
-
-    function addChild() {
-      if (parent.children.length < 10) {
-        let child = document.createElement("div");
-        let t = document.createTextNode(parent.children.length + 1);
-        child.appendChild(t);
-        child.className += "child";
-        parent.appendChild(child);
-      }
-    }
-    function deleteChild() {
-      if (parent.lastChild) {
-        parent.removeChild(parent.lastChild);
-      }
-    }
-    function setupControls() {
-      let controls = document.getElementById("controls");
-      types.forEach(function(type) {
-        let typeSection = document.createElement("div");
-        typeSection.setAttribute("class", "type-section");
-        controls.appendChild(typeSection);
-
-        classes[type].forEach(function(className) {
-          let control = document.createElement("div");
-          control.setAttribute("class", "control");
-          let label = document.createElement("label");
-          label.setAttribute("for", className);
-          label.textContent = "." + className;
-          control.appendChild(label);
-          let input = document.createElement("input");
-          input.addEventListener("change", changeCSS);
-          input.setAttribute("name", className);
-          input.setAttribute("id", className);
-          input.setAttribute("type", "checkbox");
-          control.appendChild(input);
-          typeSection.appendChild(control);
-        });
-      });
-    }
-    function setupBreakpoints() {
-      let breakpointsSection = document.getElementById("breakpoints");
-    }
-
-    function addBreakpoint() {}
-
-    function changeCSS() {
+  },
+  methods: {
+    changeCSS: function() {
       let oldStyles = document.getElementById("generated-styles");
       if (oldStyles) {
         oldStyles.remove();
       }
-      let extend = "";
-      types.forEach(function(type) {
-        classes[type].forEach(function(className) {
-          if (document.getElementById(className).checked) {
-            extend += "." + className + ",";
-          }
-        });
-      });
+    //   let extend = "";
+    //   types.forEach(function(type) {
+    //     classes[type].forEach(function(className) {
+    //       if (document.getElementById(className).checked) {
+    //         extend += "." + className + ",";
+    //       }
+    //     });
+    //   });
       let fileref = document.createElement("link");
       fileref.setAttribute("rel", "stylesheet");
       fileref.setAttribute("id", "generated-styles");
@@ -178,10 +167,8 @@ export default {
       );
       document.getElementsByTagName("head")[0].appendChild(fileref);
     }
-    for (let i = 0; i < 5; i++) {
-      addChild();
-    }
-    setupControls();
+  },
+  mounted: function() {
     changeCSS();
   }
 };
